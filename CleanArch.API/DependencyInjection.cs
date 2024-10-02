aa150dedcf59c80c3a7757e46c8a16257532c21c
A.K.M Maruful Islam <148320455+maruful106@users.noreply.github.com>
2024-10-02 22:50:10 +0600

    Setup Initial Clean Architecture Project Structure
    - Added Core project for domain models and business logic.
    - Added Application project for handling services, DTOs, and use cases.
    - Added Infrastructure project for data persistence and third-party services.
    - Added WebAPI project to expose the application via REST APIs.
    - Structured the solution to ensure separation of concerns and scalability.

---
+++ CleanArch.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CleanArch.Application;
+using CleanArch.Infrastructure;
+
+namespace CleanArch.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiDI(this IServiceCollection service)
+        {
+            service.AddApplicationDI()
+                .AddInfrastructureDI();
+            return service;
+        }
+    }
+}
